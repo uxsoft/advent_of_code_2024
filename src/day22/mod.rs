464fb4710d897927a5764295a4f005a877ea9d5e
Dryk, Jan <jan.dryk@merck.com>
2024-12-22 22:55:45 +0100

    day22 part 1

---
+++ src/day22/mod.rs
@@ -0,0 +1,36 @@
+pub mod part1;
+pub mod part2;
+
+pub struct PseudoRandomIterator {
+    secret: usize,
+}
+
+impl PseudoRandomIterator {
+    pub fn new(i: usize) -> Self {
+        Self { secret: i }
+    }
+}
+
+impl Iterator for PseudoRandomIterator {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut x = self.secret;
+
+        x = (x * 64) ^ x;
+        x %= 16777216;
+
+        x = (x >> 5) ^ x;
+        x %= 16777216;
+
+        x = (x * 2048) ^ x;
+        x %= 16777216;
+
+        self.secret = x;
+        Some(x)
+    }
+}
+
+pub fn parse(input: &str) -> Vec<usize> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
---
+++ src/day22/part1.rs
@@ -0,0 +1,47 @@
+fn compute(initial: usize, steps: usize) -> usize {
+    let mut iterator = super::PseudoRandomIterator::new(initial);
+    iterator.nth(steps - 1).unwrap()
+}
+
+pub fn process(input: &str) -> usize {
+    let numbers = super::parse(input);
+    let total = numbers.into_iter().map(|i| compute(i, 2000)).sum();
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compute_2000() {
+        let cases = vec![
+            (1, 8685429),
+            (10, 4700978),
+            (100, 15273692),
+            (2024, 8667524),
+        ];
+
+        for (initial, expected) in cases {
+            let output = compute(initial, 2000);
+            assert_eq!(output, expected);
+        }
+    }
+
+    #[test]
+    fn example() {
+        let input = "1
+10
+100
+2024";
+        let output = process(input);
+        assert_eq!(output, 37327623);
+    }
+
+    #[test]
+    fn real() {
+        let input = include_str!("input.txt");
+        let output = process(input);
+        assert_eq!(output, 0);
+    }
+}
